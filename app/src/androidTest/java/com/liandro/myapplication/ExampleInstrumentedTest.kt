5da6c61364b5b91da4965e06ed4bfb047ae4d09f
Dernival Liandro <liandro.sys@gmail.com>
2023-08-06 21:46:41 -0300

    Script login successfully

--- app/src/androidTest/java/com/liandro/myapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/liandro/myapplication/ExampleInstrumentedTest.kt
@@ -1,24 +1,36 @@
 package com.liandro.myapplication
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.closeSoftKeyboard
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 
+import org.junit.Rule
 import org.junit.Test
+
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var mActivityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.liandro.myapplication", appContext.packageName)
+    fun testLoginSucessfully() {
+        onView(withText("Welcome")).check(matches(isDisplayed()))
+        onView(withId(R.id.editTextTextEmailAddress)).perform(typeText("liandro.sys@gmail.com"))
+        closeSoftKeyboard()
+        onView(withId(R.id.editTextTextPassword)).perform(typeText("1234567"))
+        closeSoftKeyboard()
+        onView(withId(R.id.button_login)).perform(click())
+        onView(withText("Home")).check(matches(isDisplayed()))
     }
+
 }
